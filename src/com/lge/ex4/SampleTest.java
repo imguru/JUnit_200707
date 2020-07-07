838cf45553ebbbf1abf080dcc292ea59d4378b11
Chansik Yun <chansigi@gmail.com>
2020-07-07 14:23:10 +0900

    .

--- src/com/lge/ex4/SampleTest.java
+++ src/com/lge/ex4/SampleTest.java
@@ -3,10 +3,20 @@ package com.lge.ex4;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.*;
 
 public class SampleTest {
+    // 2. 비기능 테스트(속도)
+    //  : 기능적으로 잘 동작할 뿐 아니라, 테스트의 수행 시간에 대한 부분을 검증하는 작업
+
+    // 아래의 테스트가 1초 안에 수행되어야 한다.
+    @Test(timeout = 1000)
+    public void slowJobTest() throws Exception {
+        SUT.slowJob();
+    }
+
     // 1. 예외 테스트(Exception Test)
     // : parseInt에 대해서 잘못된 문자열을 전달하였을 경우, NumberFormatException 예외가 발생하는지
     //   여부를 검증하고 싶다.
@@ -16,7 +26,6 @@ public class SampleTest {
 
         SUT.parseInt(badNumberStr);
     }
-
 
     @Test
     public void parseIntTest_JUnit3() throws Exception {
@@ -34,6 +43,10 @@ public class SampleTest {
 }
 
 class SUT {
+    public static void slowJob() throws Exception {
+        TimeUnit.SECONDS.sleep(2);
+    }
+
     public static Integer parseInt(String value) throws Exception {
         // throw new IOException("123");
         return Integer.parseInt(value);
