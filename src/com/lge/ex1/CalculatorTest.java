dbc8591256889d1d16ff2ca5cc5058b2c92979bb
Chansik Yun <chansigi@gmail.com>
2020-07-07 10:51:53 +0900

    .

--- src/com/lge/ex1/CalculatorTest.java
+++ src/com/lge/ex1/CalculatorTest.java
@@ -1,6 +1,27 @@
 package com.lge.ex1;
 
-// TestCase Class
-// TestSuite Class
+// https://github.com/imguru/JUnit_200707.git
+
+// 1. JDK 8
+// https://www.oracle.com/kr/java/technologies/javase/javase-jdk8-downloads.html
+// http://bugmenot.com/view/oracle.com
+
+// 2. IntelliJ Community
+// https://www.jetbrains.com/ko-kr/idea/download/
+
+// JUnit 3 에서 Test Suite(Test Case) 를 만드는 방법.
+// public class CalculatorTest extends TestCase
+
+import org.junit.Test;
+
 public class CalculatorTest {
+
+    // JUnit 3에서 테스트 메소드는 메소드의 이름이 test로 시작해야 합니다.
+    // JUnit 4에서 테스트 메소드를 만드는 방법.
+    // => Annotation - AOP
+    @Test
+    public void testAdd() throws Exception {
+
+    }
+
 }
