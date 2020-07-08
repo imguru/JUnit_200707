af76f08fb77b0cd459e4612a15ead3aa0729b5bf
Chansik Yun <chansigi@gmail.com>
2020-07-08 14:36:44 +0900

    .

--- src/com/lge/ex11/DLoggerTest.java
+++ src/com/lge/ex11/DLoggerTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 // Test Spy Pattern
 // 의도: SUT의 메소드를 호출하였을 때, 발생하는 부수효과를 관찰 할 수 없어, 테스트 안된 요구사항이 있다.
 // 방법: 목격한 일을 기록해두었다가, 나중에 테스트에서 확인할 수 있도록 만들어진 테스트 대역
@@ -30,8 +33,37 @@ class SpyTarget implements DLoggerTarget {
 public class DLoggerTest {
     @DisplayName("DLogger에 여러개의 타겟을 등록하고, write를 수행하였을 때, 제대로 처리되었는지 여부를 검증하고 싶다.")
     @Test
-    public void writeTest() {
+    public void writeTest2() {
+        SpyTarget spy1 = new SpyTarget();
+        SpyTarget spy2 = new SpyTarget();
+        DLogger logger = new DLogger(spy1, spy2);
 
+        logger.write(Level.INFO, "test_message_info");
+        logger.write(Level.CRITICAL, "test_message_critical");
+
+        assertAll(
+                () -> assertTrue(spy1.isReceived(Level.INFO, "test_message_info"), "spy1 - info"),
+                () -> assertTrue(spy2.isReceived(Level.INFO, "test_message_info")),
+                () -> assertTrue(spy1.isReceived(Level.CRITICAL, "test_message_critical")),
+                () -> assertTrue(spy2.isReceived(Level.CRITICAL, "test_message_critical"))
+        );
+    }
+
+
+    @DisplayName("DLogger에 여러개의 타겟을 등록하고, write를 수행하였을 때, 제대로 처리되었는지 여부를 검증하고 싶다.")
+    @Test
+    public void writeTest() {
+        SpyTarget spy1 = new SpyTarget();
+        SpyTarget spy2 = new SpyTarget();
+        DLogger logger = new DLogger(spy1, spy2);
+
+        logger.write(Level.INFO, "test_message_info");
+        logger.write(Level.CRITICAL, "test_message_critical");
+
+        assertTrue(spy1.isReceived(Level.INFO, "test_message_info"));
+        assertTrue(spy2.isReceived(Level.INFO, "test_message_info"));
+        assertTrue(spy1.isReceived(Level.CRITICAL, "test_message_critical"));
+        assertTrue(spy2.isReceived(Level.CRITICAL, "test_message_critical"));
     }
 }
 
@@ -53,7 +85,7 @@ class DLogger {
 
     public void write(Level level, String message) {
         for (DLoggerTarget e : targets) {
-            e.write(level, message);
+            // e.write(level, message);
         }
     }
 }
