16bcea9028867a133c23e704298d906e6a4c4100
Chansik Yun <chansigi@gmail.com>
2020-07-08 13:35:17 +0900

    .

--- src/com/lge/ex9/TimeDisplayTest.java
+++ src/com/lge/ex9/TimeDisplayTest.java
@@ -8,16 +8,28 @@ import java.util.GregorianCalendar;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+class StubTimeProvider implements TimeProvider {
+    @Override
+    public Calendar getTime() {
+        Calendar time = new GregorianCalendar();
+
+        time.set(Calendar.HOUR_OF_DAY, 0);
+        time.set(Calendar.MINUTE, 0);
+
+        return time;
+    }
+}
+
 public class TimeDisplayTest {
     @DisplayName("자정에 Midnight의 문자열이 제대로 발생하는지 여부를 검증하고 싶다.")
     @Test
     public void getCurrentTimeAsStringTest() {
-        TimeDisplay display = new TimeDisplay();
+        StubTimeProvider stub = new StubTimeProvider();
+        TimeDisplay display = new TimeDisplay(stub);
 
         assertEquals("Midnight", display.getCurrentTimeAsString());
     }
 }
-
 
 interface TimeProvider {
     Calendar getTime();
@@ -45,7 +57,7 @@ class TimeDisplay {
     public String getCurrentTimeAsString() {
         Calendar time = provider.getTime();
 
-        int h = time.get(Calendar.HOUR);
+        int h = time.get(Calendar.HOUR_OF_DAY);
         int m = time.get(Calendar.MINUTE);
         String str = String.format("%02d:%02d", h, m);
         if (str.equals("00:00")) {
